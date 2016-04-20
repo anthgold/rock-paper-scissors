4036a14b915c7114a1b7998b9841be64bba38f00
Anthony Gold and Cory Franks <cory@>
2016-04-20 16:56:17 -0700

    broken ui home.vtl isnt linking with layout.vtl

--- src/main/java/RPS.java
+++ src/main/java/RPS.java
@@ -1,12 +1,41 @@
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
 public class RPS {
   public static void main(String[] args) {
+    staticFileLocation("/public");
     String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap model = new HashMap();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/result", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/result.vtl");
+
+      String playerOne = request.queryParams("playerOneSelect");
+      String playerTwo = request.queryParams("playerTwoSelect");
+      Integer playerValue = playerValue(playerOne, playerTwo);
+
+      model.put("result", playerValue);
+      model.put("playerOneInput", request.queryParams("playerOneSelect"));
+      model.put("playerOneInput", request.queryParams("playerTwoSelect"));
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
-  public Integer playerValue(String playerOne, String playerTwo) {
-      Integer result = 0;
-      Integer winnerPlayer1 = 1;
-      Integer winnerPlayer2 = 2;
-      Integer draw = 3;
+
+
+  public static Integer playerValue(String playerOne, String playerTwo) {
+    Integer result = 0;
+    Integer winnerPlayer1 = 1;
+    Integer winnerPlayer2 = 2;
+    Integer draw = 3;
 
     if (playerOne.equals(playerTwo)){
       result = draw;
